8d85f051a1807bee23fa169fc75350adb928940b
Giuseppe Vito Bitetti <35737374+BroHPotato@users.noreply.github.com>
2019-03-29 22:07:07 +0100

    Auto renamer for JD2

---
+++ Renamer JD2.js
@@ -0,0 +1,56 @@
+/*
+  Author: BroHPotato
+  Email:giuseppevitobitetti@libero.it
+
+  Event script : RenameFile
+
+  Change the name of all the file in the package with the name of the download folder
+  like "name of the folder - sXXeYY.type";
+  if there's subfolder in "name of the folder" named Season XX OR Specials
+  (see subdirSeason AND subdirSpecials) the script uses the name of the parent folder.
+
+  Requirements:
+    Trigger "Package Finished".
+
+  Warnings:
+    Adjust the regular expression(see regex) based on your typical download file name.
+*/
+
+//globals
+var mypackage = package; //get the package
+var links = mypackage.getDownloadLinks(); //get all the downloaded link
+var env = getEnviroment();
+var regex = /[0-9][0-9]/i;
+
+//name of the possible subdirectory
+var subdirSeason = "Season ";
+var subdirSpecials = "Specials";
+
+if(env.isWindows()) {   //Enviroment check
+    var pathsep = "\\";
+}
+else {
+    var pathsep = "/";
+}
+
+if (mypackage.isFinished()) {
+    var dir = mypackage.getDownloadFolder(); //get the package directory
+    if (dir.indexOf(subdirSeason) != -1 || dir.indexOf(subdirSpecials) != -1) { //checks if there's a subdirectory
+        if (dir.indexOf(subdirSpecials) != -1) { //if it's "Specials"
+            var nsea = "00";
+            dir = dir.substring(0, dir.length - subdirSpecials.length - 1); //get the parent directory
+        } else { //if it's "Season xx"
+            var nsea = dir.substring(dir.length - 2);
+            dir = dir.substring(0, dir.length - subdirSeason.length - 3); //get the parent directory
+        }
+    } else {
+        sea = "01";
+    }
+    dir = dir.substring(dir.lastIndexOf(pathsep) + 1); //remove the path and leave only the string with the name of the directory
+    for (var k = 0; k < links.length; k++) { //for each download
+        var name = links[k].getName();
+        var type = name.substring(name.lastIndexOf(".")); //get the extention
+        var nep = regex.exec(name); //parse the download to extract the episode
+        links[k].setName(dir + " - s" + nsea + "e" + nep + type); //change the download name in "name of the folder - sXXeYY.type"
+    }
+}
