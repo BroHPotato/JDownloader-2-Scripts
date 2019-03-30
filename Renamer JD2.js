f9f99e31e34949b132254ddc974211218a7c8110
Giuseppe Vito Bitetti <35737374+BroHPotato@users.noreply.github.com>
2019-03-30 16:23:19 +0100

    improved parser

--- Renamer JD2.js
+++ Renamer JD2.js
@@ -1,5 +1,5 @@
 /*
-  Author: Giuseppe Vito Bitetti AKA BroHPotato
+  Author: BroHPotato
   Email:giuseppevitobitetti@libero.it
 
   Event script : RenameFile
@@ -14,24 +14,25 @@
 
   Warnings:
     Adjust the regular expression(see regex) based on your typical download file name.
-    
-    Tested on Windows 10 Pro N 1809
-    Tested on Raspberry 3B+ Linux raspberrypi 4.14.98-v7+ armv71
 */
 
 //globals
 var mypackage = package; //get the package
 var links = mypackage.getDownloadLinks(); //get all the downloaded link
-var env = getEnvironment();
-var regex = /[0-9][0-9]/i;
+var env = getEnviroment();
+var firstregex = /(e|ep)(-|_| )?[0-9][0-9]/i; //ensure that the "ep" OR "e" string
+                                              //followed by the number of the episode
+                                              //is present
+var secondregex = /[0-9][0-9]/i;  //needed to parse the number of the episode
 
 //name of the possible subdirectory
 var subdirSeason = "Season ";
 var subdirSpecials = "Specials";
 
-if (env.isWindows()) { //environment check
+if(env.isWindows()) {   //Enviroment check
     var pathsep = "\\";
-} else {
+}
+else {
     var pathsep = "/";
 }
 
@@ -46,14 +47,18 @@ if (mypackage.isFinished()) {
             dir = dir.substring(0, dir.length - subdirSeason.length - 3); //get the parent directory
         }
     } else {
-        nsea = "01";
+        sea = "01";
     }
     dir = dir.substring(dir.lastIndexOf(pathsep) + 1); //remove the path and leave only the string with the name of the directory
     for (var k = 0; k < links.length; k++) { //for each download
         var name = links[k].getName();
         var type = name.substring(name.lastIndexOf(".")); //get the extention
-        var nep = regex.exec(name); //parse the download to extract the episode
+        var nep = firstregex.exec(name); //parse the download to extract the episode
+        nep = secondregex.exec(nep);
+        if (!nep) {
+          alert("Wrong episode number");
+        }
         links[k].setName(dir + " - s" + nsea + "e" + nep + type); //change the download name in "name of the folder - sXXeYY.type"
     }
-    mypackage.remove(); //remove the package
 }
+
